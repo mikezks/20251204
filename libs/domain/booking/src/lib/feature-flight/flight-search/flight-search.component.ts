953edfde8ae7fe7c685ecc405a7663a9d23d62ae
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-12-04 15:41:02 +0100

    Revert "demo: signal internals"
    This reverts commit 1617f5bd5ce6912dd29f67c4aaeea6abe21ec668.

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -5,7 +5,6 @@ import { Flight } from '../../logic-flight/model/flight';
 import { injectTicketsFacade } from '../../logic-flight/state/facade';
 import { FlightCardComponent } from '../../ui-flight/flight-card/flight-card.component';
 import { FlightFilterComponent } from '../../ui-flight/flight-filter/flight-filter.component';
-import { ReactiveNode, SIGNAL } from '@angular/core/primitives/signals';
 
 
 @Component({
@@ -36,16 +35,13 @@ export class FlightSearchComponent {
   protected flights = this.ticketsFacade.flights;
 
   constructor() {
-    let activeConsumer: ReactiveNode | null;
-    activeConsumer = effect(() => console.log(this.route()));
+    effect(() => console.log(this.route()));
 
     // Explicit Effect
     effect(() => {
       this.filter();
       untracked(() => this.search());
     });
-
-    console.log(this.route[SIGNAL]);
   }
 
   protected search(): void {
